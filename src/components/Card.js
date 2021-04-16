097fb2ae7dd83de6c731764b7eef7c325e6caef1
jim-rattagan <56734947+jim-rattagan@users.noreply.github.com>
2021-04-16 11:55:45 -0300

    cards api integration

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,25 @@
-function Card() {
-    return (
-      <div className='card'>
-        <h3>User</h3>
+import React, {Component} from "react"
+
+class Card extends Component{
+  constructor(props){
+    super(props)
+    this.state ={
+
+    }
+  }
+  render(){ 
+  return (
+      <div id={this.props.id} className='card'>
+        <h3>{this.props.userInfo.name.first + " " +this.props.userInfo.name.last}</h3>
         <img
           className='user-image'
-          src='./images/sampleimage.jpg'
+          src= {this.props.userInfo.picture.medium}
           alt='Sample User Image'
         />
-        <p>Email: harlodpain@gmail.com</p>
-        <p>Birthday: 02/02/1997 (Age 24)</p>
+        <p>Email: {this.props.userInfo.email}</p>
+        <p>Birthday: {this.props.userInfo.dob.date.substring(0,10)} (Age: {this.props.userInfo.dob.age})</p>
       </div>
     );
 }
-
+}
 export default Card
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,17 +1,46 @@
+import React, {Component} from "react"
 import Card from "./Card";
 
-function Main() {
+class Main extends Component {
+  constructor(){
+    super();
+    this.state = {
+      originalUserList: [],
+      userList: [],
+    }
+  }
+  componentDidMount(){
+    fetch("https://randomuser.me/api/?results=20")
+      .then((r) => r.json())
+      .then((resultado) => {
+        this.setState({
+          originalUserList: resultado.results,
+          userList: resultado.results,
+        });
+        console.log(this.state.userList);
+      })
+      .catch((e) => console.log(e));
+  }
+  render(){
   return (
-    <main className='wrapper'>
+   <main className='wrapper'>
       <div className='cards-wrapper'>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+      <React.Fragment>
+      {this.state.userList.map((user) => {
+            return (
+              <React.Fragment key={user.login.uuid}>
+                <Card
+                  userInfo={user}
+                  id={user.login.uuid}
+                />
+              </React.Fragment>
+            );
+          })}
+      </React.Fragment>
       </div>
     </main>
   );
+  }
 }
 
 export default Main;
