00f8b55afbc958f980c2d72ee32ddfb961ab3753
juanmartinlucioni <jlucioni@udesa.edu.ar>
2021-04-24 23:54:17 -0300

    Add + remove cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,11 +10,12 @@ class Card extends Component{
   render(){ 
   return (
       <div id={this.props.id} className='card'>
+        <button onClick={()=>{this.props.deleteCard(this.props.id)}}>x</button>
         <h3>{this.props.userInfo.name.first + " " +this.props.userInfo.name.last}</h3>
         <img
           className='user-image'
           src= {this.props.userInfo.picture.medium}
-          alt='Sample User Image'
+          alt='Sample User'
         />
         <p>Email: {this.props.userInfo.email}</p>
         <p>Birthday: {this.props.userInfo.dob.date.substring(0,10)} (Age: {this.props.userInfo.dob.age})</p>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,6 +9,33 @@ class Main extends Component {
       userList: [],
     }
   }
+  deleteCard = (idToDelete) => {
+    let userList = this.state.userList.filter(
+      (user) => user.login.uuid !== idToDelete
+    );
+    this.setState({
+      userList: userList,
+    });
+  };
+  moreCards = () => {
+    const baseUrl = "https://randomuser.me/api/?results=";
+    let addNumber = document.getElementById("numero").value 
+    fetch(baseUrl.concat(addNumber))
+      .then((r) => r.json())
+      .then((resultado) => {
+        var userListParse = this.state.userList;
+        for (let i = 0; i < resultado.results.length; i++) {
+        const user = resultado.results[i];
+        userListParse.push(user);
+        }
+        console.log(userListParse);
+        this.setState({
+          // originalUserList: resultado.results, // Para restaurar esta pagina, si quiero volver al primer state no hay que poner esta linea
+          userList: userListParse
+        });
+      })
+      .catch((e) => console.log(e));
+  };
   componentDidMount(){
     fetch("https://randomuser.me/api/?results=20")
       .then((r) => r.json())
@@ -24,6 +51,11 @@ class Main extends Component {
   render(){
   return (
    <main className='wrapper'>
+     <div className='buttons'>
+      <label for="numero"></label>
+      <input type="number" name="numero" id="numero" min="1"></input>
+      <button onClick={this.moreCards}>Add More Cards</button>
+     </div>
       <div className='cards-wrapper'>
       <React.Fragment>
       {this.state.userList.map((user) => {
@@ -32,6 +64,7 @@ class Main extends Component {
                 <Card
                   userInfo={user}
                   id={user.login.uuid}
+                  deleteCard={this.deleteCard}
                 />
               </React.Fragment>
             );
